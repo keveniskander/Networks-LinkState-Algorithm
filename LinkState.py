797d7d64fc977f7b150c526710d20342911de9de
keveniskander <keven.iskander@hotmail.com>
2020-03-29 15:02:07 -0400

    3:02

--- LinkState.py
+++ LinkState.py
@@ -3,5 +3,18 @@ f = open("test1.txt", "r")
 
 line = f.readline()
 
+nvertices = int(line.strip())
+line = f.readline()
+temp = line.split(' ')
+colcount = len(temp)
+
+print('col:', colcount)
+
+rowcount = 0
+
 while line:
-    print(line.strip())
+    rowcount +=1
+
+    line = f.readline()
+
+print('row:',rowcount)
