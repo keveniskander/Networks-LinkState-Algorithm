4fb20cf6f75b397ffd08366be537b02fe62ddc6a
keveniskander <keven.iskander@hotmail.com>
2020-03-29 17:56:07 -0400

    5:56

--- LinkState.py
+++ LinkState.py
@@ -4,24 +4,38 @@ f = open("test1.txt", "r")
 line = f.readline()
 
 nvertices = int(line.strip())
-line = f.readline()
-temp = line.split(' ')
-print(temp)
-colcount = len(temp)
+# line = f.readline()
+# temp = line.split(' ')
+# print(temp)
 
 
 rowcount = -1
 
-while line:
-    rowcount +=1
-    # print(line)
-    line = f.readline()
+for line in f:
+    rowcount += 1
 
-print('col:', colcount)
-print('row:',rowcount)
+print(rowcount)
 
-router_arr = [[0 for i in range(colcount)]for j in range(rowcount)]
+line = f.readline()
+print(line)
+temp = line.split(' ')
+print(temp)
 
-for i in range(colcount):
-    for j in range(rowcount):
-        print(router_arr[i][j])
+# print('col:', colcount)
+# print('row:',rowcount)
+
+# router_arr = [[0 for i in range(colcount)]for j in range(rowcount)]
+
+# for i in range(colcount):
+#     for j in range(rowcount):
+#         print(router_arr[i][j], end = ' ')
+#     print()
+
+
+# f = open("test1.txt", "r")
+
+# line = f.readline()
+
+# for k in range(colcount):
+#     for l in range(rowcount):
+#         router_arr[k][j] = 1
