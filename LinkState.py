f159bb4a7dcf4212f59d4271408d7373a915a1b1
keveniskander <keven.iskander@hotmail.com>
2020-03-29 15:12:26 -0400

    3:12

--- LinkState.py
+++ LinkState.py
@@ -6,15 +6,22 @@ line = f.readline()
 nvertices = int(line.strip())
 line = f.readline()
 temp = line.split(' ')
+print(temp)
 colcount = len(temp)
 
-print('col:', colcount)
 
-rowcount = 0
+rowcount = -1
 
 while line:
     rowcount +=1
-
+    # print(line)
     line = f.readline()
 
+print('col:', colcount)
 print('row:',rowcount)
+
+router_arr = [[0 for i in range(colcount)]for j in range(rowcount)]
+
+for i in range(colcount):
+    for j in range(rowcount):
+        print(router_arr[i][j])
